ddf7f4f04af882a6520dbc15f05a89798eabfc9e
Nikolay Sokolov <jentalmeister@gmail.com>
2015-11-03 19:01:44 +0300

    Fixed Stop button layout

--- InfectionsClient/Form1.Designer.cs
+++ InfectionsClient/Form1.Designer.cs
@@ -57,6 +57,7 @@
       // 
       // button2
       // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.button2.Location = new System.Drawing.Point(94, 541);
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(75, 23);
