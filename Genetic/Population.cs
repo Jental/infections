5bf639b7bb175852a82dd645eeb3f80cec4c2ec4
Nikolay Sokolov <jentalmeister@gmail.com>
2015-11-18 13:49:34 +0300

    Slower mutations

--- Genetic/Population.cs
+++ Genetic/Population.cs
@@ -98,25 +98,25 @@ namespace Genetic
 
     private int getRandomDiff()
     {
-      //int coeff = 4;
-      //int maxDiff = 8;
-      //int maxRnd = (int)Math.Pow(2, maxDiff + 1) - 1;
+      int coeff = 1;
+      int maxDiff = 8;
+      int maxRnd = (int)Math.Pow(2, maxDiff + 1) - 1;
 
-      //BitArray b = new BitArray(new byte[] { (byte)Math.Floor((double)(rnd.Next(0, maxRnd) / coeff)) });
-      //IEnumerable<bool> casted = b.Cast<bool>();
-      //for (int i = 0; i < casted.Count(); i++)
-      //{
-      //  if (casted.ElementAt(i))
-      //  {
-      //    if (i != 0)
-      //    {
-      //      int sign = (rnd.Next(0, 1) == 0) ? 1 : -1;
-      //      return sign * i * coeff;
-      //    }
-      //    else
-      //      return i * coeff;
-      //  }
-      //}
+      BitArray b = new BitArray(new byte[] { (byte)Math.Floor((double)(rnd.Next(0, maxRnd) / coeff)) });
+      IEnumerable<bool> casted = b.Cast<bool>();
+      for (int i = 0; i < casted.Count(); i++)
+      {
+        if (casted.ElementAt(i))
+        {
+          if (i != 0)
+          {
+            int sign = (rnd.Next(0, 1) == 0) ? 1 : -1;
+            return sign * (i + 1) * coeff;
+          }
+          else
+            return (i + 1) * coeff;
+        }
+      }
 
       return this.rnd.Next(-8, 8);
     }
